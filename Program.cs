113c24fa3f62f8f1926ca7e96d2d2dc652f3390f
박성원 <skfhddlg@naver.com>
2024-08-08 09:52:57 +0900

    ConsoleAppFramework 로 마이그레이션

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
-﻿using ConsoleTables;
-using System.CommandLine;
+﻿using ConsoleAppFramework;
+using ConsoleTables;
 using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.RegularExpressions;
 
-class Program
+await ConsoleApp.RunAsync(args, Main);
+
+partial class Program
 {
     /// <summary>주소로 바이트요청 후 바이트응답을 출력합니다.</summary>
     /// <param name="host">(host, port), ip, uri중 하나의 조합으로 주소를 입력해야 합니다.</param>
@@ -17,10 +19,9 @@ class Program
     /// <param name="str">전송할 문자열을 입력합니다. hex, (str, encoding)중 하나의 조합으로 입력해야 합니다.</param>
     /// <param name="encoding">문자열의 인코딩을 지정합니다. 입력하지 않을경우 시스템 기본값을 사용합니다.</param>
     /// <param name="showEncodings">입력가능한 인코딩목록을 출력합니다.</param>
-    static async Task Main(string? host = null, int? port = null, IPEndPoint? ip = null, Uri? uri = null, string? hex = null, string? str = null, string? encoding = null, bool showEncodings = false)
+    static async Task Main(string? host = null, int? port = null, [Arg] IPEndPoint? ip = null, [Arg] Uri? uri = null, string? hex = null, string? str = null, string? encoding = null, bool showEncodings = false)
     {
-        var trim_ex = new Option<int?>("aa");
-        if (showEncodings && trim_ex is { })
+        if (showEncodings)
         {
             Console.WriteLine(ConsoleTable.From(TrimEx(Encoding.GetEncodings().Select(info => new { info.Name, info.DisplayName, info.CodePage }))).ToMarkDownString());
             static IEnumerable<T> TrimEx<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicProperties)] T>(IEnumerable<T> e) => e;
@@ -70,4 +71,13 @@ partial class HexEncoding : ASCIIEncoding
     [GeneratedRegex("[^0-9a-zA-Z]")] private static partial Regex HexRegex();
     public override byte[] GetBytes(string s) => System.Convert.FromHexString(HexRegex().Replace(s, ""));
     public override string GetString(byte[] b) => System.Convert.ToHexString(b);
+}
+
+[AttributeUsage(AttributeTargets.Parameter)]
+class ArgAttribute : Attribute,
+    IArgumentParser<IPEndPoint?>,
+    IArgumentParser<Uri?>
+{
+    public static bool TryParse(ReadOnlySpan<char> s, out IPEndPoint? result) => IPEndPoint.TryParse(s, out result);
+    public static bool TryParse(ReadOnlySpan<char> s, out Uri? result) => Uri.TryCreate(new(s), uriKind: UriKind.Absolute, out result);
 }
